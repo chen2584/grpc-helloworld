e3697115f32c348eca918476050c1300bf6ad479
Worameth Semapat <worameth.semapat@gmail.com>
2020-07-24 16:10:22 +0700

    first initial

---
+++ src/GRpcExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GRpcExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/GRpcExampleServer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Grpc.Core;
+
+namespace GRpcExampleServer
+{
+    public class UserService : 
+    {
+
+    }
+
+    class Program
+    {
+        public 
+        static void Main(string[] args)
+        {
+            var server = new Server
+            {
+                Services = Services,
+                Ports = { new ServerPort("0.0.0.0", "2584", ServerCredentials.Insecure); }
+            };
+        }
+    }
+}
