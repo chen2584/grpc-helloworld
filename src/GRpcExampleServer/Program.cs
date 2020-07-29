bb456bbe8abd1ebfef0a41b4e27cd3b23d4d071f
Worameth Semapat <worameth.semapat@gmail.com>
2020-07-29 09:57:40 +0700

    Added Server

--- src/GRpcExampleServer/Program.cs
+++ src/GRpcExampleServer/Program.cs
@@ -1,23 +1,44 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Grpc.Core;
 
 namespace GRpcExampleServer
 {
-    public class UserService : 
+    public class UserService : global::UserService.UserServiceBase
     {
+        public override Task<ResponseUser> GetUserDetail(RequestUser request, ServerCallContext context)
+        {
+            var zeroTime = new DateTime(1, 1, 1);
+            var span = DateTime.Now - request.BirthDay.ToDateTime();
+            var age = (zeroTime + span).Year - 1;
 
+            var response = new ResponseUser
+            {
+                Message = $"Hello {request.Name}",
+                Age = age
+            };
+            return Task.FromResult(response);
+        }
     }
 
     class Program
     {
-        public 
         static void Main(string[] args)
         {
+            var port = 2584;
+
             var server = new Server
             {
-                Services = Services,
-                Ports = { new ServerPort("0.0.0.0", "2584", ServerCredentials.Insecure); }
+                Services = { global::UserService.BindService(new UserService()) },
+                Ports = { new ServerPort("0.0.0.0", port, ServerCredentials.Insecure) }
             };
+            server.Start();
+
+            Console.WriteLine("Greeter server listening on port " + port);
+            Console.WriteLine("Press any key to stop the server...");
+            Console.ReadKey();
+
+            server.ShutdownAsync().Wait();
         }
     }
 }
